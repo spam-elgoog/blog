c8577c2b9ddf4028fb35712a1687c34be0ea9fc1
Peter Studnicka <peter.studnicka@shopify.com>
2019-11-20 16:20:00 -0500

    Seed database changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,23 @@
 require_relative 'data_helper'
 
-def random_amount(range)
-  rand(range) * 1.01
-end
-
-def add_budget_details(plan)
-  cats = Category.all
-  cats.each do |cat|
-    # Get random amount for eacch category
-    amount =  case cat.category.to_sym
-              when :ads, :apps then random_amount 75..150
-              when :refunds, :damages, :misc then random_amount 100..300
-              when :transportation, :permits then random_amount 200..400
-              when :storage then random_amount 800..1500
-              # supplies, refunds, utils, website and anything I missed
-              else random_amount 50..200
-              end
-    plan.budget_details.create(category_id: cat.id, amount: amount)
-  end
-end
-
+# Categories Table
+Category.connection.execute 'ALTER TABLE `categories` AUTO_INCREMENT = 1'
 populate_categories
 
-users = populate_users
+# Users Table
+User.connection.execute 'ALTER TABLE `users` AUTO_INCREMENT = 1'
+populate_users
 
-# leave last 3 records with no Budget Plans to start so there are users
-# with no budgets available.
-users[0..(users.length - 3)].each do |x|
-  # initialize a November budget
-  add_budget_details x.budget_plans.create(plan_date: Date.new(2019, 11, 1))
-  # every second user initialize an October budget also
-  if x.id.even?
-    add_budget_details x.budget_plans.create(plan_date: Date.new(2019, 10, 1))
-  end
-  puts 'INFO: Budget_Plans and Budget_Details table populated.'
-end
+# Budget_Plans Table
+BudgetPlan.connection.execute 'ALTER TABLE `budget_plans` AUTO_INCREMENT = 1'
+populate_plans
+
+# Budget_Details Table
+@plans = BudgetPlan.all
+BudgetDetail.connection
+            .execute 'ALTER TABLE `budget_details` AUTO_INCREMENT = 1'
+populate_details @plans
+
+# Expenses Table
+Expense.connection.execute 'ALTER TABLE `expenses` AUTO_INCREMENT = 1'
+populate_expenses @plans
