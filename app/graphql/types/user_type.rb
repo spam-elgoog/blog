f073f60cd93464d04bf61063ab51c8161b1b4515
Peter Studnicka <peter.studnicka@shopify.com>
2019-12-15 22:56:13 -0500

    last update

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 module Types
-    class UserType < Types::BaseObject
-      # Add root-level fields here.
-      # They will be entry points for queries on your schema.
-      field :id, ID, null: false, description: "Auto Generated ID"
-      field :f_name, String, null: false, description: ""
-      field :l_name, String, null: false, description: ""
-      field :email, String, null: false, description: ""
-      field :user_name, String, null: false, description: ""
-    end
+  # User type
+  class UserType < Types::BaseObject
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+    field :id, ID, null: false, description: 'Auto Generated ID'
+    field :f_name, String, null: false, description: ''
+    field :l_name, String, null: false, description: ''
+    field :email, String, null: false, description: ''
+    field :user_name, String, null: false, description: ''
+  end
 end
